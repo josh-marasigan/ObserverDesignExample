c016aae6c41d5193258af4f64e612b497401c47b
Josh Marasigan <josh.marasigan@toyotaconnected.com>
2018-04-28 12:58:01 -0500

    Initial Commit

---
+++ observer-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  observer-example
+//
+//  Created by Josh Marasigan on 4/28/18.
+//  Copyright © 2018 Josh Marasigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
